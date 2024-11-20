14632c86e8de05a0727dbc47c5dec70d222edcef
Riki Itokazu <itokazuriki@gmail.com>
2024-11-19 22:31:39 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Riki Itokazu on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
