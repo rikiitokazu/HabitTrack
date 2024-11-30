7dc29bf7bd3adcd9e21450db06e66d5795885931
Riki Itokazu <itokazuriki@gmail.com>
2024-11-30 01:28:47 -0500

    chore: more cleanup

--- FinalProject/Views/CameraViewBack.swift
+++ FinalProject/Views/CameraViewBack.swift
@@ -25,16 +25,15 @@ struct CameraViewBack: View {
                     .ignoresSafeArea()
                 
                 CountdownView(variation: .back, countdownFinished: $countdownFinished, VM: $VM)
-//                horizontalControlBar
-//                    .frame(height: controlFrameHeight)
             }
             .onAppear() {
                 print("on appear triggered")
                 VM.requestAcccessAndSetup(position: .back)
             }
             .onChange(of: countdownFinished) {
-                // Wait 0.5 seconds, then set the VM.photo to the photo
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                // TODO: refactor this part
+                // Wait 1 seconds, then set the VM.photo to the photo
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                     if VM.photoData == nil {
                         print("photo has not finished uploading")
                         return
@@ -46,85 +45,6 @@ struct CameraViewBack: View {
         }
     }
     
-//    private var cameraPreview: some View {
-//        GeometryReader { geo in
-//            CameraPreview(cameraVM: $VM, frame: geo.frame(in: .global))
-//                .onAppear() {
-//                    VM.requestAcccessAndSetup(position: cameraDisplay == .back ? .back : .front)
-//                }
-//        }
-//        .ignoresSafeArea()
-//    }
-    
-    @ViewBuilder private var horizontalControlBar: some View {
-        if VM.hasPhoto {
-            horizontalControlBarPostPhoto
-        } else {
-            horizontalControlBarPrePhoto
-        }
-    }
-    
-    private var horizontalControlBarPrePhoto: some View {
-        HStack {
-             cancelButton
-                .frame(width: controlButtonWidth)
-             Spacer()
-             photoCaptureButton
-             Spacer()
-            Spacer()
-                .frame(width: controlButtonWidth)
-        }
-    }
-    
-    private var horizontalControlBarPostPhoto: some View {
-        HStack {
-            retakeButton
-                .frame(width: controlButtonWidth)
-            Spacer()
-            usePhotoButton
-                .frame(width: controlButtonWidth)
-        }
-    }
-    
-    private var usePhotoButton: some View {
-        ControlButtonView(label: "Use Photo") {
-            backPhoto = VM.photoData
-
-        }
-    }
-    
-    private var retakeButton: some View {
-        ControlButtonView(label: "Retake") {
-            VM.retakePhoto()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                withAnimation {
-                    VM.takePhoto()
-                    // boolean to show 1 or 2 view
-                }
-            }
-        }
-    }
-    
-    private var cancelButton: some View {
-        ControlButtonView(label: "Cancel") {
-            showCamera = false
-        }
-    }
-    
-    private var photoCaptureButton: some View {
-        Button {
-            VM.takePhoto()
-        } label: {
-            ZStack {
-                Circle()
-                    .fill(.white)
-                    .frame(width: 65)
-                Circle()
-                    .stroke(.white, lineWidth: 3)
-                    .frame(width: 75)
-            }
-        }
-    }
 }
 
 #Preview {
--- FinalProject/Views/CameraViewFront.swift
+++ FinalProject/Views/CameraViewFront.swift
@@ -25,8 +25,6 @@ struct CameraViewFront: View {
                 Color.black
                     .ignoresSafeArea()
                 CountdownView(variation: .front, countdownFinished: $countdownFinished, VM: $VM)
-//                horizontalControlBar
-//                    .frame(height: controlFrameHeight)
             }
             .fullScreenBackCamera(isPresented: $showBackView, cameraDisplay: $cameraDisplay, backPhoto: $backPhoto, showCamera: $showCamera)
             
@@ -37,7 +35,7 @@ struct CameraViewFront: View {
             .onChange(of: countdownFinished) {
                 // TODO: wait until the photoData has loaded. if its not, maybe
                 // just do a ProgressView()
-                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
                     if VM.photoData == nil {
                         print("photo has not finished uploading")
                         return
@@ -49,85 +47,7 @@ struct CameraViewFront: View {
         }
     }
     
-    //    private var cameraPreview: some View {
-    //        GeometryReader { geo in
-    //            CameraPreview(cameraVM: $VM, frame: geo.frame(in: .global))
-    //                .onAppear() {
-    //                    VM.requestAcccessAndSetup(position: cameraDisplay == .back ? .back : .front)
-    //                }
-    //        }
-    //        .ignoresSafeArea()
-    //    }
-    
-    @ViewBuilder private var horizontalControlBar: some View {
-        if VM.hasPhoto {
-            horizontalControlBarPostPhoto
-        } else {
-            horizontalControlBarPrePhoto
-        }
-    }
-    
-    private var horizontalControlBarPrePhoto: some View {
-        HStack {
-            cancelButton
-                .frame(width: controlButtonWidth)
-            Spacer()
-            photoCaptureButton
-            Spacer()
-            Spacer()
-                .frame(width: controlButtonWidth)
-        }
-    }
-    
-    private var horizontalControlBarPostPhoto: some View {
-        HStack {
-            retakeButton
-                .frame(width: controlButtonWidth)
-            Spacer()
-            usePhotoButton
-                .frame(width: controlButtonWidth)
-        }
-    }
-    
-    private var usePhotoButton: some View {
-        ControlButtonView(label: "Use Photo") {
-            frontPhoto = VM.photoData
-            showCamera = false
-        }
-    }
-    
-    private var retakeButton: some View {
-        ControlButtonView(label: "Retake") {
-            VM.retakePhoto()
-            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                withAnimation {
-                    VM.takePhoto()
-                    // boolean to show 1 or 2 view
-                }
-            }
-        }
-    }
-    
-    private var cancelButton: some View {
-        ControlButtonView(label: "Cancel") {
-            showCamera = false
-        }
-    }
-    
-    private var photoCaptureButton: some View {
-        Button {
-            VM.takePhoto()
-        } label: {
-            ZStack {
-                Circle()
-                    .fill(.white)
-                    .frame(width: 65)
-                Circle()
-                    .stroke(.white, lineWidth: 3)
-                    .frame(width: 75)
-            }
-        }
-    }
+
 }
 
 extension View {    
