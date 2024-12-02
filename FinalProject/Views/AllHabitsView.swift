5dfc056c27cfb364dc8c2a9b4c8ba2eeb740a187
Riki Itokazu <itokazuriki@gmail.com>
2024-12-02 02:16:45 -0500

    feat: ui for main view

--- FinalProject/Views/AllHabitsView.swift
+++ FinalProject/Views/AllHabitsView.swift
@@ -13,63 +13,128 @@ struct AllHabitsView: View {
     @State private var imageIsLoading = false
     @State private var selectedPhoto: PhotosPickerItem?
     @State private var data = Data()
-    @State private var pickerIsPresented = true
+    @State private var pickerIsPresented = false
     @State private var selectedImage = Image(systemName: "photo")
     var body: some View {
-        List(dummyHabitPhotos, id: \.self) { photo in
-            LazyVStack (alignment: .leading) {
-                VStack (alignment: .leading) {
-                    Text("username")
-                        .foregroundStyle(.white)
-                    Text("11/8/4 4:52 PM")
-                        .foregroundStyle(.white)
-                }
-                Button("Save image") {
-                    Task {
-                        await HabitPhotoViewModel.saveImage(habit: Habit(), photo: HabitPhoto(), data: data, display: .front)
-                    }
-                }
-                ZStack {
-                    Color.clear
-  
-                    selectedImage
-                        .resizable()
-                        .frame(width: 10, height: 10)
+        VStack {
+            VStack {
+                HStack (alignment: .center){
+                    Spacer()
                     
-                    // replaced to // if photoisLoading
-                    if imageIsLoading {
-                        VStack(alignment: .center) {
-                            Rectangle()
-                                .fill(.black600)
-                                .frame(width: 330, height: 430)
-                        }
-                    } else {
-                        ZStack (alignment: .topLeading) {
-                            Image(systemName: "photo")
-                                .resizable()
-                                .frame(width: 330, height: 430)
-                                .border(.blue300, width: 3)
-                                
-                            Image(systemName: "camera.fill")
-                                .resizable()
-                                .foregroundStyle(.blue800)
-                                .frame(width: 100, height: 100)
-                                .border(.blue300, width: 3)
-                        }
+                    HStack (spacing: 0) {
+                        Text("Habit")
+                            .foregroundStyle(.white)
+                        Text("Track.")
+                            .foregroundStyle(.blue400)
+                    }
+                    .font(.title)
+                    .bold()
+                    .padding(.leading, 40)
+                    
+
+                    
+                    Spacer()
+                    
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundStyle(.white)
+                    
+                }
+                .padding([.trailing, .leading, .bottom])
+                .padding(.top, 10)
+                
+                HStack {
+                    Button {
+                        // trigger separate view
+                    } label: {
+                        Text("Your Habits")
+                            .font(.title3)
+                            .foregroundStyle(.gray)
+                            .overlay {
+                                Rectangle()
+                                    .frame(height: 1)
+                                    .foregroundStyle(.clear)
+                                    .frame(maxHeight: .infinity, alignment: .bottom)
+                                    .padding([.top], 30)
+                            }
+                    }
+                    
+                    Spacer()
+                        .frame(width: 50)
+                    
+                    Button {
+                        // trigger separate view
+                    } label: {
+                        Text("Discovery")
+                            .font(.title3)
+                            .foregroundStyle(.white)
+                            .overlay {
+                                Rectangle()
+                                    .frame(height: 2)
+                                    .foregroundStyle(.blue400)
+                                    .frame(maxHeight: .infinity, alignment: .bottom)
+                                    .padding([.top], 30)
+                            }
+                    }
+                    
                 }
             }
-     
-            .frame(maxWidth: .infinity) // Expand ZStack to full width
-            Text("Caption goes here....")
-                .foregroundStyle(.white)
+            .background(.black700)
+            List(dummyHabitPhotos, id: \.self) { photo in
+                LazyVStack (alignment: .leading) {
+                    VStack (alignment: .leading) {
+                        Text("username")
+                            .foregroundStyle(.white)
+                        Text("11/8/4 4:52 PM")
+                            .foregroundStyle(.white)
+                    }
+                    Button("Save image") {
+                        Task {
+                            await HabitPhotoViewModel.saveImage(habit: Habit(), photo: HabitPhoto(), data: data, display: .front)
+                        }
+                    }
+                    ZStack {
+                        Color.clear
+                        
+                        selectedImage
+                            .resizable()
+                            .frame(width: 10, height: 10)
+                        
+                        // replaced to // if photoisLoading
+                        if imageIsLoading {
+                            VStack(alignment: .center) {
+                                Rectangle()
+                                    .fill(.black600)
+                                    .frame(width: 330, height: 430)
+                            }
+                        } else {
+                            ZStack (alignment: .topLeading) {
+                                Image(systemName: "photo")
+                                    .resizable()
+                                    .frame(width: 330, height: 430)
+                                    .border(.blue300, width: 3)
+                                
+                                Image(systemName: "camera.fill")
+                                    .resizable()
+                                    .foregroundStyle(.blue800)
+                                    .frame(width: 100, height: 100)
+                                    .border(.blue300, width: 3)
+                            }
+                        }
+                    }
+                    
+                    .frame(maxWidth: .infinity) // Expand ZStack to full width
+                    Text("Caption goes here....")
+                        .foregroundStyle(.white)
+                }
+                .padding()
+                .task {
+                    //                await creatures.loadNextIfNeeed(creature: creature)
+                }
+                .listRowBackground(Color(.black700))
+            }
         }
-
-        .padding()
-        .task {
-            //                await creatures.loadNextIfNeeed(creature: creature)
-        }
-        .listRowBackground(Color(.black700))
-    }
         .background(.black700)
         .listStyle(.plain)
         
@@ -78,10 +143,10 @@ struct AllHabitsView: View {
             // turn selectedPhoto into imageView
             Task {
                 do {
-                   if let image = try await selectedPhoto?.loadTransferable(type: Image.self)
+                    if let image = try await selectedPhoto?.loadTransferable(type: Image.self)
                     {
-                       selectedImage = image
-                   }
+                        selectedImage = image
+                    }
                     guard let transferredData = try await selectedPhoto?.loadTransferable(type: Data.self) else {
                         print("error could not convert data from selected Photo")
                         return
@@ -92,13 +157,13 @@ struct AllHabitsView: View {
                 }
             }
         }
-    
-    //        if creatures.isLoading {
-    //            ProgressView()
-    //                .tint(.red)
-    //                .scaleEffect(4)
-    //        }
-}
+        
+        //        if creatures.isLoading {
+        //            ProgressView()
+        //                .tint(.red)
+        //                .scaleEffect(4)
+        //        }
+    }
 }
 
 #Preview {
