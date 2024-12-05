40caf78d4412f9ceb43d99f7007c49ae6f08ff5c
Riki Itokazu <itokazuriki@gmail.com>
2024-12-05 13:31:13 -0500

    feat: personal habits view ui

--- FinalProject/Views/PersonalHabitsView.swift
+++ FinalProject/Views/PersonalHabitsView.swift
@@ -20,19 +20,56 @@ struct PersonalHabitsView: View {
     
     var body: some View {
         List(habits) { habit in
-            NavigationLink {
-                CreateHabitView(habit: habit)
-            } label: {
-                Text(habit.habitName)
-            }
-            .swipeActions {
-                Button("Delete", role: .destructive) {
-                    HabitViewModel.deleteHabit(habit: habit)
+            VStack {
+                NavigationLink {
+                    CreateHabitView(habit: habit)
+                } label : {
+                    VStack (alignment: .leading){
+                        HStack (alignment: .center){
+                            Image(systemName: "camera")
+                            
+                            Text(habit.habitName)
+                                .bold()
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.5)
+                            
+                        }
+                        .padding([.bottom], 3)
+                        
+                        HStack (spacing: 0) {
+                            Text("Completed:")
+                                .font(.caption)
+                                .foregroundStyle(.white)
+                                .bold()
+                            Text(" \(habit.completedForTheDay)/\(habit.frequency.rawValue)")
+                                .font(.caption)
+                            
+                        }
+                        
+                        HStack (spacing: 0){
+                            Text("Last Completed:")
+                                .foregroundStyle(.white)
+                                .font(.caption)
+                                .bold()
+                            
+                            Text(" \(habit.lastCompleted?.formatted(date: .numeric, time: .shortened) ?? habit.dateCreated.formatted(date: .numeric, time: .shortened))")
+                                .font(.caption)
+                        }
+                    }
                 }
+                .swipeActions {
+                    Button("Delete", role: .destructive) {
+                        HabitViewModel.deleteHabit(habit: habit)
+                    }
+                }
+                .foregroundStyle(.white)
+                
             }
+            .listRowBackground(Color(.black700))
         }
+        .background(.black800)
         .listStyle(.plain)
-        .navigationTitle("\(user.name != "" ? user.name : "no name") habits")
+        .foregroundStyle(.white)
         .toolbar {
             ToolbarItem (placement: .topBarLeading) {
                 Button("Back") {
@@ -40,6 +77,7 @@ struct PersonalHabitsView: View {
                 }
             }
         }
+        
     }
     
 }
