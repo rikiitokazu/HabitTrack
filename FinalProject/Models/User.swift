6c93dd12b496c6ffc772b135f1169cfabb019ff5
Riki Itokazu <itokazuriki@gmail.com>
2024-12-04 19:15:30 -0500

    chore: cleanup and profile view

--- FinalProject/Models/User.swift
+++ FinalProject/Models/User.swift
@@ -12,12 +12,17 @@ import FirebaseAuth
 class User: Identifiable, Codable {
     
     @DocumentID var id: String?
+    var email: String
+    var name: String
     var userId: String
     var lastLogin: Date
     var totalMissed: Int
     var totalAccomplished: Int
     
-    init(id: String? = nil, userId: String = (Auth.auth().currentUser?.uid ?? ""), lastLogin: Date = Date.now, totalMissed: Int = 0, totalAccomplished: Int = 0) {
+    init(id: String? = nil, email: String = "", name: String = "", userId: String = (Auth.auth().currentUser?.uid ?? ""), lastLogin: Date = Date.now, totalMissed: Int = 0, totalAccomplished: Int = 0) {
+        self.id = id
+        self.email = email
+        self.name = name
         self.userId = userId
         self.lastLogin = lastLogin
         self.totalMissed = totalMissed
--- FinalProject/Views/ProfileView.swift
+++ FinalProject/Views/ProfileView.swift
@@ -9,21 +9,74 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "person.crop.circle.fill")
-                .resizable()
-//                .symbolRenderingMode()
-                .frame(width: 100, height: 100)
-            Button("Change Profile Picture") {
-                
+         VStack {
+            HStack {
+                VStack (alignment: .leading){
+                    Text("John Doe")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .lineLimit(1)
+                        .bold()
+                        .minimumScaleFactor(0.5)
+                    Text("\("johndoe@email.com")")
+                        .foregroundStyle(.white)
+                        .italic()
+                }
+                Spacer()
+                VStack {
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                    //                .symbolRenderingMode()
+                        .frame(width: 100, height: 100)
+                    
+                    Button("Edit") {
+                        
+                    }
+                }
             }
+            
+            Spacer()
+                .frame(height: 70)
+            
+            VStack (alignment: .leading) {
+                HStack (spacing: 0) {
+                    Text("\(18)")
+                        .font(.title)
+                        .foregroundStyle(.blue200)
+                        .foregroundStyle(.white)
+                    Text(" Current Habits")
+                        .font(.title2)
+                        .foregroundStyle(.white)
+                }
+                
+            
+                Spacer()
+                    .frame(height:70)
+                
+                HStack (spacing: 0){
+                    Text("\(20)")
+                        .foregroundStyle(.green)
+                    Text(" Completed Habits")
+                        .foregroundStyle(.white)
+                }
+                
+                HStack (spacing: 0){
+                    Text("\(3)")
+                        .foregroundStyle(.red)
+                    Text(" Missed Habits")
+                        .foregroundStyle(.white)
+                }
+                
+
+            }
+            Spacer()
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.black700)
         .toolbar {
             ToolbarItem(placement: .topBarTrailing) {
                 Button("Back") {
-                   // TODO: go back
+                    // TODO: go back
                 }
             }
         }
@@ -31,5 +84,7 @@ struct ProfileView: View {
 }
 
 #Preview {
-    ProfileView()
+    NavigationStack {
+        ProfileView()
+    }
 }
